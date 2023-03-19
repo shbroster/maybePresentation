7adc1df19d796261a53327ccf005e6c2117a12c0
shbroster <10621010+shbroster@users.noreply.github.com>
2023-03-19 11:12:54 +0000

    Deploying to gh-pages from @ shbroster/maybePresentation@6f628aa785f79562d487dfe64b3913a4aad1218c 🚀

--- assets/PresenterPrint-98252a3e.js
+++ assets/PresenterPrint-98252a3e.js
@@ -1,4 +1,4 @@
-import{d as _,i as d,a as p,u as h,b as u,c as m,e as f,f as n,g as t,t as s,h as a,F as g,r as v,n as x,j as y,o as r,k as b,l as k,m as N,p as P,q as w,_ as S}from"./index-ecb62d4a.js";import{N as V}from"./NoteDisplay-6af49e36.js";const j={class:"m-4"},L={class:"mb-10"},T={class:"text-4xl font-bold mt-2"},B={class:"opacity-50"},C={class:"text-lg"},D={class:"font-bold flex gap-2"},H={class:"opacity-50"},z=t("div",{class:"flex-auto"},null,-1),F={key:0,class:"border-gray-400/50 mb-8"},M=_({__name:"PresenterPrint",setup(q){d(p),h(`
+import{d as _,i as d,a as p,u as h,b as u,c as m,e as f,f as n,g as t,t as s,h as a,F as g,r as v,n as x,j as y,o as r,k as b,l as k,m as N,p as P,q as w,_ as S}from"./index-2d6269e4.js";import{N as V}from"./NoteDisplay-cbc067c1.js";const j={class:"m-4"},L={class:"mb-10"},T={class:"text-4xl font-bold mt-2"},B={class:"opacity-50"},C={class:"text-lg"},D={class:"font-bold flex gap-2"},H={class:"opacity-50"},z=t("div",{class:"flex-auto"},null,-1),F={key:0,class:"border-gray-400/50 mb-8"},M=_({__name:"PresenterPrint",setup(q){d(p),h(`
 @page {
   size: A4;
   margin-top: 1.5cm;
